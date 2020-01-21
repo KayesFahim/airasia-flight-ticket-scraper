c98a1d0e8fc91920ea20f69db98fd1a924932932
Luis yChun <yongchun903@gmail.com>
2020-01-21 22:01:16 +0800

    add list of references in README, add time sleep for full page rendering

---
+++ README.md
@@ -0,0 +1,5 @@
+### Reference
+- [Data Science Skills: Web scraping javascript using python](https://towardsdatascience.com/data-science-skills-web-scraping-javascript-using-python-97a29738353f)
+- [Better web scraping in Python with Selenium, Beautiful Soup, and pandas](https://www.freecodecamp.org/news/better-web-scraping-in-python-with-selenium-beautiful-soup-and-pandas-d6390592e251/)
+- [Web Scraping Using Selenium — Python](https://towardsdatascience.com/web-scraping-using-selenium-python-8a60f4cf40ab)
+- [Beginner's guide to web scraping with python's selenium](https://dev.to/lewiskori/beginner-s-guide-to-web-scraping-with-python-s-selenium-3fl9)
--- scraper.py
+++ scraper.py
@@ -1,11 +1,28 @@
 from selenium.webdriver import Chrome
 import pandas as pd
+from bs4 import BeautifulSoup
+import time
 
 webdriver = r'/usr/local/bin/chromedriver'
 
 driver = Chrome(executable_path=webdriver)
 
 # URL = 'http://quotes.toscrape.com/js/page/1/'
-URL = 'https://www.airasia.com/select/en/gb/JHB/PEN/2020-04-30/N/1/0/0/O/N/MYR/ST?key=4e8837c2-3b85-11ea-bd63-c724cbdfa8891579525432.25'
+URL = 'https://www.airasia.com/select/en/gb/JHB/PEN/2020-04-30/N/1/0/0/O/N/MYR/ST'
 
 driver.get(URL)
+
+# execute script to scroll down the page
+driver.execute_script(
+    "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+# sleep for 30s
+time.sleep(10)
+
+price_element = driver.find_element_by_xpath(
+    '//*[@id="lf-low-amount-desc-0-0"]')
+
+price = price_element.text
+
+print(price)
+
+driver.quit()
